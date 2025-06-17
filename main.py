ec9da215fd714c34b58fcaa218783008652ce102
joze3000 <mihatitov4@gmail.com>
2025-06-17 20:53:41 +0300

    git push

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from dataclasses import dataclass, field
 from enum import Enum, auto
-from random import randint, choice
+from random import randint, choice, random
 import logging
 
 #Нужно выполнить задание по логгированию, добавить макс хп, перемещение по локациям, инвентарь и получение предметов, восстановление здоровья, шансы побега.
-#class Main(Enum):
+
+
 class Area(Enum):
     plain  =    auto()
     forest =    auto()
     mountain =  auto()
     swamps =    auto()
     ruins  =    auto()
+
+area_name = {
+    Area.plain :    "Равнины",
+    Area.forest :   "Лес",
+    Area.mountain:  "Горы",
+    Area.swamps:    "Болота",
+    Area.ruins:     "Руины"
+}
 
 area_lvl = {
     Area.plain:    range(1,5),
@@ -27,6 +36,7 @@ area_enemy =  {
     Area.swamps:    ["Слизняк","Пиявка","Мертвяк"],
     Area.ruins:     ["Боевая кукла","Оживший меч","Скелет-воин"]
 }
+
 @dataclass
 class Enemy():
     name: str
@@ -38,7 +48,8 @@ class Enemy():
     def damage(self, skill_dmg = 3):
         return int(((self.lvl + 1) * (skill_dmg + randint(-3,3)))/2)
     def __del__(self):
-        logging.debug(f"Бедный моб погиб")
+        logging.error(f"Бедный моб погиб")
+
 @dataclass
 class Player():
     name: str
@@ -58,83 +69,120 @@ class Player():
             self.lvl += 1
             self.experience_limit += int(self.experience*0.3)
             logging.info(f"Поздравляем с получением {self.lvl} уровня")       
+
 def main():
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     file_handler = logging.FileHandler('battle.log')
     file_handler.setFormatter(logging.Formatter('%(message)s'))
     console_handler = logging.StreamHandler()
-    console_handler.setFormatter(logging.Formatter('%(message)s'))
+    console_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
+    p_info = logging.info
     player = Player("Игрок", 1, 100)
+    
     found_enemy  = Enemy(choice(area_enemy[player.location]), choice(area_lvl[player.location]), 100)
-    def new_enemy():
-        del found_enemy
-        logging.critical("Пытаемся удалить врага")
-        found_enemy  = Enemy(choice(area_enemy[player.location]), choice(area_lvl[player.location]), 100)
+    #Декоратор для нового врага
+    def new_choise(combat):
+        travel_choise = input("""Так или иначе ты прошел чуть глубже по локе.
+    Вариантов стало куда больше:
+1 - Слегка отдохнуть(восстановит немного хп)
+2 - Продолжить сражения
+3 - Перейти на следующий уровень      
+    """)
+        if travel_choise == 1:
+            p_info("Неплохо посидели, подумали о вечном. Вроде как даже отдохнули немного")
+        elif travel_choise == 2:
+            p_info("Фига ты неугомонный фармила. Ну дерзай")
+            found_enemy  = Enemy(choice(area_enemy[player.location]), choice(area_lvl[player.location]), 100)
+            combat(player, found_enemy)
+        elif travel_choise == 3:
+            player.location = Area((player.location.value)+1)
+            p_info(f"""Смело. Ну удачи выжить.
+Ты вышел на новую локацию - {area_name[player.location]}
+                   """)
+            return new_choise(combat)
+        else:
+            p_info("Давай не безобразничай тут.")
+        p_info(f"Пытаемся удалить врага")
+    #сырая функция выдачи лута
+    def give_loot():
+        pass
+    #Сырая функция побега
+    def try_escape() -> str: 
+        p_info("Попытка побега")
+        return 'dead' if randint(0, 1) == 0 else 'escape'
+
     def set_player_name(name):
         if name:
             player.name = name
-            logging.info(f"Понятно. Очередной {player.name}")
+            p_info(f"Понятно. Очередной {player.name}")
         else:
             player.name = "Игрок"
-            logging.info("Тогда будешь стандартным Игроком\n")
-    def combat(player: Player, found_enemy  : Enemy) -> str:
+            p_info("Тогда будешь стандартным Игроком\n")
+    def combat(player: Player, found_enemy: Enemy) -> str:
         while player.hp > 0 and found_enemy.hp > 0:
             player_dmg = player.damage()
             enemy_dmg = found_enemy.damage()
             player.hp -= enemy_dmg
             found_enemy.hp -= player_dmg
-            logging.info(f"{player.name} Уебал {found_enemy.name} на {player_dmg} единиц урона. Но получил ответку на {enemy_dmg} единиц урона по лицу")
+            p_info(f"{player.name} Уебал {found_enemy.name} на {player_dmg} единиц урона. Но получил ответку на {enemy_dmg} единиц урона по лицу")
             if player.hp <= 0:
-                logging.info("Йоу, ты умер.")
+                p_info("Йоу, ты умер.")
                 return "dead"
             elif found_enemy.hp <= 0:
-                logging.info("Туда его. Красава! Завалил гниду.")
+                p_info("Туда его. Красава! Завалил гниду.")
                 return "win"
             else:    
-                logging.info(f"У вас осталось {player.hp} здоровья. У вражины {found_enemy .hp}.Что делаем?\n 1 - сражайся как мужчина\n 2 - в ужасе сьеби в туман")
+                p_info(f"""У вас осталось {player.hp} здоровья. У вражины {found_enemy .hp}.
+Что делаем?
+1 - сражайся как мужчина 
+2 - в ужасе сьеби в туман""")
                 fight_choise = input()
                 if fight_choise == '1':
-                    logging.info("Продолжаем файт")
+                    p_info("Продолжаем файт")
                 #TODO Стоит добавить шанс на побег. *В следующих версиях
                 elif fight_choise == '2':
-                    logging.info("Ты тут вообще то в сражении. Развернувшись ты тут же получил удра в хребет и умер. Лошара")
-                    return "dead"
+                    p_info("Ты тут вообще то в сражении. Развернувшись ты тут же получил удра в хребет и умер. Лошара")
+                    return try_escape()
                 else:
-                    logging.info("Ты что, тупой? Выбирай нормально")
-    
-    logging.info("TEST!!!!!!!!!!!")
-    logging.info(f"Привет! Сегодня ты возьмешь на себя роль беттатестера угрюмой хуеты, которая может стать игрой. Выбора особого у тебя конечно же нет.")
+                    logging.error("Ты что, тупой? Выбирай нормально")
+                       
+    p_info(f"""Привет! Сегодня ты возьмешь на себя роль беттатестера угрюмой хуеты, которая может стать игрой.
+Выбора особого у тебя конечно же нет.""")
     name_p =  input("Ну что дружище, как звать то тебя?\n")
     set_player_name(name_p)
     #Внезапная генерация врага
-    logging.info(f"""Ты оказался на равнинах, вокруг раскинулись луга. Ляпота.
+    p_info(f"""Ты оказался на равнинах, вокруг раскинулись луга. Ляпота.
 Внезапно!
-Ты встречаешь {found_enemy .name}
+Ты встречаешь {found_enemy.name}
 Так как вариантов у тебя немного, оставляю тебе 2 выбора:
 Вариант 1 - ты сражаешься
 Вариант 2 - ты умираешь от голода""")
     while True:
         variant = input("Выбирай с умом. И не надо лишнего. Либо 1, либо 2.\n")
         if variant == '1':
-            logging.info("Ну давай, заебашь его")
+            p_info("Ну давай, заебашь его")
             buttle_result = combat(player, found_enemy)
             if buttle_result == "dead":
-                logging.info("Записали тебя в позорники")
+                p_info("Записали тебя в позорники")
                 return 0
             elif buttle_result == "win":
-                logging.info("Ай Маладец!")
-                logging.info(f"Вы получили {found_enemy .give_exp} опыта")
-                player.try_lvl_up(found_enemy .give_exp)
+                p_info("Ай Маладец!")
+                p_info(f"Вы получили {found_enemy.give_exp} опыта")
+                del found_enemy
+                player.try_lvl_up(found_enemy.give_exp)
+                new_choise(combat())
+            elif buttle_result == "escape":
+                p_info("Своим позорным побегом ты никого не удивил, но хоть жив остался...")
                 return 0
             else:
-                logging.info("Попытайся снова")
+                logging.error("Попытайся снова")
         elif variant == '2':
-            logging.info("Глупо умер от изнеможения. Смех да и только.")
+            p_info("Глупо умер от изнеможения. Смех да и только.")
             break
         else:
-            logging.info("Зря ты так. Я ведь тебя просил не исполнять")
+            p_info("Зря ты так. Я ведь тебя просил не исполнять")
 if __name__ == "__main__":
     main()
